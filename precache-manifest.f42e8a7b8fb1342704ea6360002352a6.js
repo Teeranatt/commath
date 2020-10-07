6e5f7a7e6ed51cafab163477538f87b43247c444
Teeranatt <teeranat.sr.61@ubu.ac.th>
2020-10-07 18:21:28 +0700

    Updates

---
+++ precache-manifest.f42e8a7b8fb1342704ea6360002352a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e065ddd1466033df1d8fe9234336da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0d8361ce798cf62e644",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "158218816fedd2cc5e9d",
+    "url": "/static/js/2.05004212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05004212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d8361ce798cf62e644",
+    "url": "/static/js/main.09de3b5b.chunk.js"
+  },
+  {
+    "revision": "c83c666ac3710ba77d65",
+    "url": "/static/js/runtime-main.049373c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
